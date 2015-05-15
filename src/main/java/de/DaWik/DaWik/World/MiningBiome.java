8f8fb6e4f8ebe804d64d2ed35fad636615911a00
mcNETDev <mcfrek99@gmail.com>
2015-05-15 14:59:16 +0200

    fix permissions bug

--- src/main/java/de/DaWik/DaWik/World/MiningBiome.java
+++ src/main/java/de/DaWik/DaWik/World/MiningBiome.java
@@ -43,14 +43,14 @@ public class MiningBiome extends BiomeGenBase {
 	}
 
 	@Override
-	public void decorate(World par1World, Random par2Random, int par3, int par4) {
-		super.decorate(par1World, par2Random, par3, par4);
+	public void decorate(World par1World, Random par2Random, int X, int Y) {
+		super.decorate(par1World, par2Random, X, Y);
 		int var5 = par2Random.nextInt(2);
 		WorldGenMinable gen = new WorldGenMinable(Blocks.emerald_ore, 0, 3, Blocks.stone);
 		for (int var6 = 0; var6 < var5; var6++) {
-			int var7 = par3 + par2Random.nextInt(16);
+			int var7 = X + par2Random.nextInt(16);
 			int var8 = par2Random.nextInt(28) + 4;
-			int var9 = par4 + par2Random.nextInt(16);
+			int var9 = Y + par2Random.nextInt(16);
 			Block var10 = par1World.getBlock(var7, var8, var9);
 			gen.generate(par1World, par2Random, var7, var8, var9);
 		}
--- src/main/java/de/DaWik/DaWik/util/Player.java
+++ src/main/java/de/DaWik/DaWik/util/Player.java
@@ -9,6 +9,6 @@ public class Player {
 				return true;
 			}
 		}
-		return true;
+		return false;
 	}
 }
